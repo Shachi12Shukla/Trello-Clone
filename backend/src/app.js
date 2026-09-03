4f42de6390754b843f659706489bee5070b99e6e
Shachi Shukla <shachi.03shukla@gmail.com>
2026-09-04 00:40:07 +0530

    connected deployed frontend to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,12 +10,19 @@ const app = express();
 app.use(express.json());
 
 const allowedOrigins = [
-    "http://localhost:5173"
+    "http://localhost:5173",
+    process.env.FRONTEND_URL
 ];
 
 app.use(
     cors({
-        origin: allowedOrigins,
+        origin: function (origin, callback) {    
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
         credentials: true
     })
 );
